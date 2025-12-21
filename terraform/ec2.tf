abda7bfda80927587dfe62e89d70a4d23bad528a
Your Name <sivan@Sivas>
2025-12-21 20:49:49 +0530

    argocd in cluster with t3.medium

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "k3s" {
   ami = "ami-0f58b397bc5c1f2e8" # Ubuntu 22.04 (ap-south-1)
-  instance_type = "t3.micro"
+  instance_type = "t3.medium"
   key_name      = var.key_name
   vpc_security_group_ids = [aws_security_group.k3s_sg.id]
   # user_data = file("${path.module}/bootstrap/bootstrap.sh")
